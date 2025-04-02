6e8a47101a9e4c72a41b95b156893e748cb3f2dd
basgenix <basgenix2@gmail.com>
2025-04-02 01:04:10 -0700

    Add files via upload

---
+++ FUWMARKET PLACE/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdhUToigY6Ew7nd4YbuX6pO-YfIzUrQrA",
+  authDomain: "fuwmarketplace-a5cbd.firebaseapp.com",
+  projectId: "fuwmarketplace-a5cbd",
+  storageBucket: "fuwmarketplace-a5cbd.firebasestorage.app",
+  messagingSenderId: "754997773414",
+  appId: "1:754997773414:web:71ad0c1a323a12748399a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
